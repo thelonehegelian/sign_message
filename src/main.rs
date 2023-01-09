c2fda716cdb2b96608b06ae4236b006f65a376e5
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-09 08:07:26 +0000

    ADd some comments

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use ring::{digest, hmac, rand};
 fn main() {
     // create a range
     let rng = rand::SystemRandom::new();
-    // create an array of key_value
+    // create an array of key_value, the standard length of Sha256
     let key_value: [u8; digest::SHA256_OUTPUT_LEN] = rand::generate(&rng).unwrap().expose();
-    // fill the key_value array with random numbers
     // create a key
     let key = hmac::Key::new(hmac::HMAC_SHA384, key_value.as_ref());
     // message
