cc603ad74d87e8c4fbd99e23a475388cd43208d9
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-09 08:04:52 +0000

    Create key, sign and verify message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ring::{digest, hmac, rand};
 fn main() {
-    println!("Hello, world!");
+    // create a range
+    let rng = rand::SystemRandom::new();
+    // create an array of key_value
+    let key_value: [u8; digest::SHA256_OUTPUT_LEN] = rand::generate(&rng).unwrap().expose();
+    // fill the key_value array with random numbers
+    // create a key
+    let key = hmac::Key::new(hmac::HMAC_SHA384, key_value.as_ref());
+    // message
+    let message = "Columbo is on to him";
+    // sign the message
+    let signature = hmac::sign(&key, message.as_bytes());
+    // verify the signature
+    hmac::verify(&key, message.as_bytes(), signature.as_ref()).unwrap();
 }
